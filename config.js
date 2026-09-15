2dd064e4f6a3b6961ba9e441278a6720b21a9185
trupe-HyZs <missing.hendril@gmail.com>
2026-09-15 09:11:29 -0300

    Criar site oficial do pemBOLÃO

---
+++ config.js
@@ -0,0 +1,6 @@
+// COLOQUE AQUI os dados públicos do seu projeto Supabase.
+// Não coloque a service_role key aqui. Use somente a chave "anon/public".
+window.PEMBOLAO_CONFIG = {
+  url: "COLE_A_URL_DO_SEU_PROJETO_AQUI",
+  anonKey: "COLE_A_CHAVE_ANON_PUBLIC_AQUI"
+};
